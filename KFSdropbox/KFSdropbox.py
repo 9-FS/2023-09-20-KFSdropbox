5810860f347d01f2e1a229fd4a86745768f3d194
9FS <pray4spam@googlemail.com>
2024-05-04 17:57:16 +0000

    updated copyright notice year 2023 to year 2024

--- KFSdropbox/KFSdropbox.py
+++ KFSdropbox/KFSdropbox.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+# Copyright (c) 2024 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
 import dropbox.dropbox_client, dropbox.exceptions, dropbox.files
 import os
 import requests
